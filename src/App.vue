591cd9f48afb90aae8334b8ea67a310c1edf09e5
Derek Mason <3derekmason@gmail.com>
2022-08-02 08:58:35 -0600

    form init

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,13 @@
     <h1>Oh for <code>fs</code> sake...</h1>
   </header>
 
-  <main></main>
+  <main>
+    <h1>Upload Image</h1>
+    <form>
+      <input type="file" name="newFile" accept="image/*, .md" />
+      <button type="submit">UPLOAD</button>
+    </form>
+  </main>
 </template>
 
 <style scoped></style>
