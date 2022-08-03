e54040d64af7b288e938c65631218727b2f613f3
Derek Mason <3derekmason@gmail.com>
2022-08-03 09:43:02 -0600

    configure mongoose and save to db

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,20 @@
 // require the installed packages
 import express from "express";
+import bodyParser from "body-parser";
 import multer from "multer";
 import cors from "cors";
+import mongoose from "mongoose";
+import fs from "fs";
 import dotenv from "dotenv";
 
 dotenv.config();
-import mongodb from "mongodb";
-import fs from "fs";
-
-const PORT = 5000;
 
 //CREATE EXPRESS APP
 const app = express();
+// eslint-disable-next-line no-undef
+const myurl = process.env.MONGODB_URI;
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 5000;
 
 var corsOptions = {
   origin: "*",
@@ -20,19 +23,15 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
-// DATABASE CONNECTION
-const MongoClient = mongodb.MongoClient;
-// eslint-disable-next-line no-undef
-const myurl = process.env.MONGODB_URI;
-let db;
+app.use(bodyParser.urlencoded({ extended: true }));
 
-MongoClient.connect(myurl, (err, client) => {
-  if (err) return console.log(err);
-  db = client.db("forfssake");
-  app.listen(3000, () => {
-    console.log("Database connected successfully");
-  });
+// DATABASE CONNECTION
+mongoose.connect(myurl);
+//File Schema
+const fileSchema = mongoose.Schema({
+  file: { data: "Buffer", contentType: String },
 });
+const newFile = mongoose.model("file", fileSchema);
 
 // SET STORAGE
 var storage = multer.diskStorage({
@@ -51,15 +50,25 @@ app.get("/api", (req, res) => {
   res.json({ message: "WELCOME" });
 });
 
-app.post("/api/upload", upload.single("file"), (req, res, next) => {
-  const file = req.file;
-  console.log(file);
-  if (!file) {
-    const error = new Error("Please upload a file");
-    error.httpStatusCode = 400;
-    return next(error);
-  }
-  res.send(file);
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  var file = fs.readFileSync(req.file.path);
+
+  var encode_file = file.toString("base64");
+  var final_file = {
+    contentType: req.file.mimetype,
+    file: encode_file.toString("base64"),
+  };
+  console.log(final_file);
+  newFile.create(final_file, function (err, result) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(result);
+      console.log("Saved To database");
+      res.contentType(final_file.contentType);
+      res.send(final_file);
+    }
+  });
 });
 
 app.listen(PORT, () =>
